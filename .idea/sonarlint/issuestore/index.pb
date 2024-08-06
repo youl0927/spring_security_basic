fc86a7ebe2571e2aecf0e4920dfa35176e26b6ae
spring_JPA <believethink@naver.com>
2024-08-06 22:26:34 +0900

    start

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+m
+=src/main/java/com/example/demo/controller/MainController.java,a/8/a8080dd3a7c3c8aece4db685727f1416f0b11c83
+n
+>src/main/java/com/example/demo/controller/AdminController.java,2/1/21e10e8e3cdd5edc461b0761d09e55df5e509e21
+r
+Bsrc/main/java/com/example/demo/SpringSecurityBasicApplication.java,1/d/1d400dc761559359af7a61f0d5b65e0bb06908f3
+i
+9src/main/java/com/example/demo/config/SecurityConfig.java,e/8/e803ae3906c0397c95d1136f4b32a2792d7c9605
+e
+5src/main/java/com/example/demo/entity/UserEntity.java,6/d/6d7f844e23f4c48ad129eb2367fc0c71e620288d
+m
+=src/main/java/com/example/demo/repository/UserRepository.java,d/3/d3988bae5a67c550f3f8baf357a9c936160c7052
---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity      //이 빈은 Security bean이라고 선언해주는 것
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();     //모든 비밀번호들을 검증할때 해쉬로 암호화해서 검증할꺼다
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //csrf disable      -> csrf 공격은 세션 관련된 공격인가봄, jwt는 방어할 필요가 없으니 disable해둔다고함..
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()        //이쪽 경로는 모두 허용
+                        .requestMatchers("/admin").hasRole("ADMIN")                 //이쪽은 어드민만
+                        .anyRequest().authenticated());                                       //이쪽은 로그인한 모든 사용자
+
+        //세션 설정
+        http
+                .sessionManagement((session)-> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));           //세션방식 안쓸거임
+
+        return http.build();
+    }
+}
